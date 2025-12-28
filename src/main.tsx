fc53cb8d0b6b56b6a47f8b11fadb1409d1aab18a
G <gauravgol34@gmail.com>
2025-12-28 22:15:53 +0530

    error fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
